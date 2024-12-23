696f1a0c1c2b0b55137f5f6216fd88273527f01d
Jayesh Thore <jayeshthore7@gmail.com>
2024-12-23 20:23:27 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,35 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-5rem]'>
+          <svg
+            className='relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(60%-30rem)] sm:h-[65rem]'
+            viewBox='0 0 1100 800'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fill='url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)'
+              fillOpacity='.3'
+              d='M217.219 218.975L203.852 978 0 138.341l317.219 10.634 304.172-286.402c1.307 2632.337 45.083 46.658 209.733 145.248C936.936 126.058 882.053-94.234 1131.02 41.331c119.18 1308.451 1130.68 295.337 21.53 375.223L855 299l21.173 462.054-258.954-242.079z'
+            />
+            <defs>
+              <linearGradient
+                id='45de2b6b-92d5-4d68-a6a0-9b9b2abad533'
+                x1='1155.49'
+                x2='-78.208'
+                y1='.177'
+                y2='474.645'
+                gradientUnits='userSpaceOnUse'
+              >
+                <stop stopColor='#9089FC' />
+                <stop offset={1} stopColor='#FF80B5' />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
